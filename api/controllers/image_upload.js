4d2fc5b9a5c5c54774efaf0f7dd1138b2c6114aa
bharatpatil21 <patilbharat09@gmail.com>
2017-04-29 10:49:02 +0530

    Corrected folder path name.

--- api/controllers/image_upload.js
+++ api/controllers/image_upload.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var util = require('util');
-var fs = require("fs");
+let util = require('util');
+let fs = require("fs");
 
 module.exports = {
   uploadImage: uploadImage
@@ -11,12 +11,12 @@ function uploadImage(req, res) {
   let reqPath = req.swagger.params.uploadfile.originalValue;
   let path = './uploads/';
 
-  if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
+  if (!fs.existsSync(path)){
+    fs.mkdirSync(path);
   }
   fs.writeFile( path + reqPath.originalname, reqPath.buffer , (err) => {
     if (err) {
-      var err = { message: 'File not uploaded' };
+      let err = { message: 'File not uploaded' };
       return next(err);
     }
     res.json("File uploaded");
